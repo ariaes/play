1ab852bd395126f5e197d5061b3be9ac85c74443
noel.rodriguez <noel.rodriguez@blueliv.com>
2022-08-18 13:46:33 -0400

    Tying visual testing with github actions

--- tests/visualtest.test.js
+++ tests/visualtest.test.js
@@ -25,7 +25,7 @@ describe(`UI tests for dynamic content using playwright and applitools`, () => {
 
     test(`Should look ok`, async () => {
         await page.waitForSelector('h3', { state: 'attached' });
-        await eyes.open(page, 'The internet', 'Dynamic content', new RectangleSize(800, 600));
+        await eyes.open(page, 'The internet', 'Dynamic content', new RectangleSize(800, 800));
         await eyes.check(Target.window().fully());
         await eyes.close();
 
